82d56bf30bde9d0d62c00323c65cb75f82519c73
Wilder Trujillo <nadie437@gmail.com>
2024-01-21 00:35:02 -0500

    inicial de java

---
+++ src/main/java/com/buscacode/admin/buscacodeadmin/BuscacodeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.buscacode.admin.buscacodeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscacodeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscacodeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buscacode/admin/buscacodeadmin/BuscacodeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buscacode.admin.buscacodeadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscacodeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
